71ae68fb422e3ca100889c6f2b34ce32e0e6e8a5
Brady Klockenga and kevin garvey <brady@>
2019-03-26 14:38:13 -0700

    Add ability to toggle answers on and off

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(this).addClass("showAnswer");
-    $(this).addClass("showQuestion");
+    $(this).find('.answer').toggle();
+    $(this).find('.question').toggle();
   });
 });

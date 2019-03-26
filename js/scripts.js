97b843f28095146253b8c207f7b776122968320f
Brady Klockenga and kevin garvey <kevin@>
2019-03-26 13:16:20 -0700

    add first six flashcards with css and jquery

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#initially-showing1").toggle();
+    $("#initially-hidden1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#initially-showing2").toggle();
+    $("#initially-hidden2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $("#initially-showing3").toggle();
+    $("#initially-hidden3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable4").click(function() {
+    $("#initially-showing4").toggle();
+    $("#initially-hidden4").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable5").click(function() {
+    $("#initially-showing5").toggle();
+    $("#initially-hidden5").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable6").click(function() {
+    $("#initially-showing6").toggle();
+    $("#initially-hidden6").toggle();
+  });
+});
